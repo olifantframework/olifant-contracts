05577b370555a9f3c97cdc65fcfabe08b8704d72
undercloud <lodashes@gmail.com>
2019-02-27 23:27:58 +0400

    Delete RequestHandlerInterface.php

--- src/Http/RequestHandlerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Olifant\Contracts\Http;
-
-use Psr\Http\Server\RequestHandlerInterface as PsrRequestHandlerInterface;
-
-interface RequestHandlerInterface extends PsrRequestHandlerInterface
-{
-
-}
