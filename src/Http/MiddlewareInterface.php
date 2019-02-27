1d5de7e58cc9efe9ef0074cc73db428903490a39
undercloud <lodashes@gmail.com>
2019-02-27 23:27:46 +0400

    Delete MiddlewareInterface.php

--- src/Http/MiddlewareInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Olifant\Contracts\Http;
-
-use Psr\Http\Server\MiddlewareInterface as PsrMiddlewareInterface;
-
-interface MiddlewareInterface extends PsrMiddlewareInterface
-{
-
-}
