7f977f37a6cd74041753a90e48def7d72da95862
undercloud <lodashes@gmail.com>
2019-02-27 23:28:21 +0400

    Delete ResponseInterface.php

--- src/Http/ResponseInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Olifant\Contracts\Http;
-
-use Psr\Http\Message\ResponseInterface AS PsrResponseInterface;
-
-interface ResponseInterface extends PsrResponseInterface, CookieResponseInterface
-{
-
-}
