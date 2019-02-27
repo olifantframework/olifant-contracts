01faca782fed5ae460bfc07ed1950e41917abda1
undercloud <lodashes@gmail.com>
2019-02-27 23:27:32 +0400

    Delete CookieResponseInterface.php

--- src/Http/CookieResponseInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Olifant\Contracts\Http;
-
-interface CookieResponseInterface extends CookieInterface
-{
-	public function withCookie($name, $value, array $options = []);
-	public function withForgetCookie($name);
-}
